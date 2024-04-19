bfee402c9c5591e2d3433cd73b73e4ad48279598
uttam <uttam@192.168.1.2>
2024-04-19 09:08:08 +0545

    middleware cleared

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -1,4 +1,19 @@
 import type { NextAuthConfig } from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
+import credentials from "next-auth/providers/credentials";
 
-export default { providers: [GitHub] } satisfies NextAuthConfig;
+export default {
+  providers: [
+    GitHub,
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import authConfig from "./app/auth.config";
-import NextAuth from "next-auth";
-
-export const { auth: middleware } = NextAuth(authConfig);
-
-// Optionally, don't invoke Middleware on some paths
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-};
